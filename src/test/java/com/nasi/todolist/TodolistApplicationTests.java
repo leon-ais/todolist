6189fa17e4843c41012ded96cf3f3891b3cdae73
张磊 <295211889@qq.com>
2020-02-17 12:45:50 +0800

    创建springboot项目todolist。

---
+++ src/test/java/com/nasi/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasi.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
